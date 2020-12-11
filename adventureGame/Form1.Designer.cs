4ff57d74cd3eb788b3013b3ae2f90cd4b0f35c91
Dawson Reimer <dawsreim708@outlook.com>
2020-12-11 14:44:52 -0500

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace adventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textOutput = new System.Windows.Forms.Label();
+            this.question1 = new System.Windows.Forms.Label();
+            this.question2 = new System.Windows.Forms.Label();
+            this.question3 = new System.Windows.Forms.Label();
+            this.mButton = new System.Windows.Forms.PictureBox();
+            this.nButton = new System.Windows.Forms.PictureBox();
+            this.bButton = new System.Windows.Forms.PictureBox();
+            this.rx7Image = new System.Windows.Forms.PictureBox();
+            this.challengerImage = new System.Windows.Forms.PictureBox();
+            this.image240sx = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rx7Image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.challengerImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image240sx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textOutput
+            // 
+            this.textOutput.BackColor = System.Drawing.Color.Black;
+            this.textOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textOutput.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.ForeColor = System.Drawing.Color.White;
+            this.textOutput.Location = new System.Drawing.Point(32, 35);
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(366, 175);
+            this.textOutput.TabIndex = 0;
+            // 
+            // question1
+            // 
+            this.question1.BackColor = System.Drawing.Color.Black;
+            this.question1.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question1.ForeColor = System.Drawing.Color.White;
+            this.question1.Location = new System.Drawing.Point(96, 254);
+            this.question1.Name = "question1";
+            this.question1.Size = new System.Drawing.Size(366, 32);
+            this.question1.TabIndex = 1;
+            // 
+            // question2
+            // 
+            this.question2.BackColor = System.Drawing.Color.Black;
+            this.question2.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question2.ForeColor = System.Drawing.Color.White;
+            this.question2.Location = new System.Drawing.Point(96, 309);
+            this.question2.Name = "question2";
+            this.question2.Size = new System.Drawing.Size(366, 32);
+            this.question2.TabIndex = 2;
+            // 
+            // question3
+            // 
+            this.question3.BackColor = System.Drawing.Color.Black;
+            this.question3.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question3.ForeColor = System.Drawing.Color.White;
+            this.question3.Location = new System.Drawing.Point(96, 363);
+            this.question3.Name = "question3";
+            this.question3.Size = new System.Drawing.Size(366, 32);
+            this.question3.TabIndex = 3;
+            // 
+            // mButton
+            // 
+            this.mButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mButton.BackgroundImage")));
+            this.mButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mButton.Location = new System.Drawing.Point(32, 254);
+            this.mButton.Name = "mButton";
+            this.mButton.Size = new System.Drawing.Size(58, 32);
+            this.mButton.TabIndex = 4;
+            this.mButton.TabStop = false;
+            // 
+            // nButton
+            // 
+            this.nButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nButton.BackgroundImage")));
+            this.nButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.nButton.Location = new System.Drawing.Point(32, 363);
+            this.nButton.Name = "nButton";
+            this.nButton.Size = new System.Drawing.Size(58, 32);
+            this.nButton.TabIndex = 5;
+            this.nButton.TabStop = false;
+            // 
+            // bButton
+            // 
+            this.bButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bButton.BackgroundImage")));
+            this.bButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bButton.Location = new System.Drawing.Point(32, 309);
+            this.bButton.Name = "bButton";
+            this.bButton.Size = new System.Drawing.Size(58, 32);
+            this.bButton.TabIndex = 6;
+            this.bButton.TabStop = false;
+            // 
+            // rx7Image
+            // 
+            this.rx7Image.BackgroundImage = global::adventureGame.Properties.Resources.rx7;
+            this.rx7Image.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.rx7Image.Location = new System.Drawing.Point(462, 35);
+            this.rx7Image.Name = "rx7Image";
+            this.rx7Image.Size = new System.Drawing.Size(299, 175);
+            this.rx7Image.TabIndex = 7;
+            this.rx7Image.TabStop = false;
+            this.rx7Image.Visible = false;
+            // 
+            // challengerImage
+            // 
+            this.challengerImage.BackgroundImage = global::adventureGame.Properties.Resources.challenged;
+            this.challengerImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.challengerImage.Location = new System.Drawing.Point(462, 35);
+            this.challengerImage.Name = "challengerImage";
+            this.challengerImage.Size = new System.Drawing.Size(299, 175);
+            this.challengerImage.TabIndex = 8;
+            this.challengerImage.TabStop = false;
+            this.challengerImage.Visible = false;
+            // 
+            // image240sx
+            // 
+            this.image240sx.BackgroundImage = global::adventureGame.Properties.Resources._240sx;
+            this.image240sx.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.image240sx.Location = new System.Drawing.Point(462, 35);
+            this.image240sx.Name = "image240sx";
+            this.image240sx.Size = new System.Drawing.Size(299, 175);
+            this.image240sx.TabIndex = 9;
+            this.image240sx.TabStop = false;
+            this.image240sx.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 416);
+            this.Controls.Add(this.image240sx);
+            this.Controls.Add(this.challengerImage);
+            this.Controls.Add(this.rx7Image);
+            this.Controls.Add(this.bButton);
+            this.Controls.Add(this.nButton);
+            this.Controls.Add(this.mButton);
+            this.Controls.Add(this.question3);
+            this.Controls.Add(this.question2);
+            this.Controls.Add(this.question1);
+            this.Controls.Add(this.textOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.mButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rx7Image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.challengerImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image240sx)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textOutput;
+        private System.Windows.Forms.Label question1;
+        private System.Windows.Forms.Label question2;
+        private System.Windows.Forms.Label question3;
+        private System.Windows.Forms.PictureBox mButton;
+        private System.Windows.Forms.PictureBox nButton;
+        private System.Windows.Forms.PictureBox bButton;
+        private System.Windows.Forms.PictureBox rx7Image;
+        private System.Windows.Forms.PictureBox challengerImage;
+        private System.Windows.Forms.PictureBox image240sx;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace adventureGame
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+        bool mazdaRx7;
+        bool nissan240;
+        bool dodgeChallenger;
+        int chance;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+            {
+                if (scene == 1)
+                {
+                    scene = 2;
+                    mazdaRx7 = true;
+                    rx7Image.Visible = true;
+                }
+                else if (scene == 2)
+                {
+                    scene = 3;
+                }
+                else if (scene == 3)
+                {
+                    if (dodgeChallenger == true)
+                    {
+                        scene = 6;
+                    }
+                    else
+                    {
+                        scene = 5;
+                    }
+                }
+                else if (scene == 4)
+                {
+                    Random randNum = new Random();
+                    chance = randNum.Next(1, 101);
+                    if (chance <= 90)
+                    {
+                        scene = 11;
+                    }
+                    else
+                    {
+                        scene = 10;
+                    }
+                }
+                else if (scene == 5)
+                {
+                    scene = 8;
+                }
+                else if (scene == 7)
+                {
+                    Random randNum = new Random();
+                    chance = randNum.Next(1, 101);
+                    if (chance >= 90)
+                    {
+                        scene = 11;
+                    }
+                    else
+                    {
+                        scene = 10;
+                    }
+                }
+                else if (scene == 9)
+                {
+                    scene = 12;
+                }
+                else if (scene == 13)
+                {
+                    scene = 14;
+                }
+                else if (scene == 15)
+                {
+                    scene = 16;
+                }
+                else if (scene == 16)
+                {
+                    scene = 18;
+                }
+                else if (scene == 18)
+                {
+                    scene = 99;
+                }
+                else if (scene == 100)
+                {
+                    Application.Exit();
+                }
+
+            }
+            if (e.KeyCode == Keys.B)
+            {
+                if (scene == 1)
+                {
+                    scene = 2;
+                    nissan240 = true;
+                    image240sx.Visible = true;
+                }
+                else if (scene == 2)
+                {
+                    scene = 97;
+                }
+                else if (scene == 3)
+                {
+                    scene = 4;
+                }
+                else if (scene == 4)
+                {
+                    scene = 9;
+                }
+                else if (scene == 5)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
+                    scene = 9;
+                }
+                else if (scene == 9)
+                {
+                    scene = 13;
+                }
+                else if (scene == 13)
+                {
+                    scene = 99;
+                }
+                else if (scene == 15)
+                {
+                    scene = 97;
+                }
+                else if (scene == 16)
+                {
+                    scene = 17;
+                }
+                else if (scene == 18)
+                {
+                    scene = 99;
+                }
+                else if (scene == 100)
+                {
+                    scene = 1;
+                }
+            }
+            if (e.KeyCode == Keys.N)
+            {
+                if (scene == 1)
+                {
+                    scene = 2;
+                    dodgeChallenger = true;
+                    challengerImage.Visible = true;
+                }
+            }
+
+            switch (scene)
+            {
+                case 1:
+                    textOutput.Text = "Choose your car.";
+
+                    question1.Text = "1997 Mazda RX7";
+                    question2.Text = "1994 Nissan 240SX";
+                    question3.Text = "2020 Dodge Challenger";
+                    question3.Visible = true;
+                    nButton.Visible = true;
+                    mButton.Visible = true;
+                    bButton.Visible = true;
+                    break;
+                case 2:
+                    textOutput.Text = "You are driving casually at a high speed and a police officer flashes his lights, what do you do?";
+
+                    question1.Text = "Try and run";
+                    question2.Text = "Pull over";
+                    question3.Visible = false;
+                    nButton.Visible = false;
+                    break;
+                case 3:
+                    textOutput.Text = "You start accelerating away, you notice you can turn left down a road. Do you turn or go straight?";
+
+                    question1.Text = "Turn";
+                    question2.Text = "Go Straight";
+                    break;
+                case 4:
+                    textOutput.Text = "You keep going straight but the cop is catching up, you notice a store you can hide behind, what do you do?";
+
+                    question1.Text = "Hide behind store";
+                    question2.Text = "Keep going straight";
+                    break;
+                case 5:
+                    textOutput.Text = "you turn and start to oversteer, do you slow down to stop sliding or do you try and drift?";
+
+                    question1.Text = "Drift";
+                    question2.Text = "Slow down";
+                    break;
+                case 6:
+                    textOutput.Text = "You tried to turn but died because your in a dodge challenger";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 98;
+                    break;
+                case 7:
+                    textOutput.Text = "you slow down and correct your turn and the cop is getting close, you notice a store you can hide behind, what do you do?";
+
+                    question1.Text = "Hide";
+                    question2.Text = "Keep driving away";
+                    break;
+                case 8:
+                    textOutput.Text = "You pull off a smooth drift as you see the cop slide into the building on the corner";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 99;
+                    break;
+                case 9:
+                    textOutput.Text = "You are diving straight as the cop tries to push your back end out";
+
+                    question1.Text = "try speeding up";
+                    question2.Text = "turn around and go the other way";
+                    break;
+                case 10:
+                    textOutput.Text = "The cop drives by and doesn't see you";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 99;
+                    
+                    break;
+                case 11:
+                    textOutput.Text = "The cop drives by and doesn't see you";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 15;
+                    
+                    break;
+                case 12:
+                    textOutput.Text = "you failed at trying to accelerate straight and you spun out and crashed";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 97;
+                    break;
+                case 13:
+                    textOutput.Text = "you accelerate in the opposite direction and you see an unknown road you can go down";
+
+                    question1.Text = "take unknown road";
+                    question2.Text = "keep going straight";
+                    break;
+                case 14:
+                    textOutput.Text = "The road is a dead end";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 97;
+                    break;
+                case 15:
+                    textOutput.Text = "You Escaped";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 99;
+                    break;
+                case 16:
+                    textOutput.Text = "You start driving away as you see another cop parked up ahead what do you do?";
+
+                    question1.Text = "Turn around";
+                    question2.Text = "Keep going";
+
+                    break;
+                case 17:
+                    textOutput.Text = "The cop throws spike onto the road causing your tires to pop, you lose control and slide into a semi truck.";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    mButton.Visible = false;
+                    bButton.Visible = false;
+                    Refresh();
+                    Thread.Sleep(4000);
+                    scene = 98;
+                    break;
+                case 18:
+                    textOutput.Text = "the cop tries to hit you as you turn around but misses, you escaped";
+
+                    question1.Text = "drift as you drive away";
+                    question2.Text = "don't drift";
+                    break;
+                case 97:
+                    textOutput.Text = "you lose";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    Refresh();
+                    Thread.Sleep(5000);
+                    scene = 100;
+                    break;
+                case 98:
+                    textOutput.Text = "you died";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    Refresh();
+                    Thread.Sleep(5000);
+                    scene = 100;
+                    break;
+                case 99:
+                    textOutput.Text = "you win";
+
+                    question1.Text = "";
+                    question2.Text = "";
+                    Refresh();
+                    Thread.Sleep(5000);
+                    scene = 100;
+                    Refresh();
+                    break;
+                case 100:
+                    textOutput.Text = "Play again?";
+
+                    question1.Text = "no";
+                    question2.Text = "yes";
+                    break;
+            }
+            
+
+        }
+
+    }
+}
